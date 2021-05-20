d3d82cff73e6131c5957545ba6c13713671d7975
Michael Hsieh <michaelhsieh42@gmail.com>
2021-05-20 09:39:16 +0000

    - ml exmple

--- ml_example/breast_cancer_modeling.r
+++ ml_example/breast_cancer_modeling.r
@@ -1,5 +1,22 @@
-install.packages(c('caret', 'mlbench'))
+install.packages(c('caret', 'mlbench', 'tidyverse'))
 library(caret)
 library(mlbench)
 
 data(BreastCancer)
+summary(BreastCancer) #Summary of Dataset
+
+df <- BreastCancer
+# convert input values to numeric
+for(i in 2:10) {
+  df[,i] <- as.numeric(as.character(df[,i]))
+}
+
+
+trainIndex <- createDataPartition(df$Class, p = .8, 
+                                  list = FALSE, 
+                                  times = 1)
+df_train <- df[ trainIndex,]
+df_test  <- df[-trainIndex,]
+preProcValues <- preProcess(df_train, method = c("center", "scale", "medianImpute"))
+df_train_transformed <- predict(preProcValues, df_train)
+df_test_transformed <- predict(preProcValues, df_test)
